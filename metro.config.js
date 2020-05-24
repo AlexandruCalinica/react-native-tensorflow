75ef26e950c1ce8c16e225827df62bb7c21c6ad0
AlexandruCalinica <calinicaalexandru@gmail.com>
2020-05-24 14:08:38 +0300

    Added tensorflowjs + peer dependencies in the correct way

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,12 @@
+const blacklist = require('metro-config/src/defaults/blacklist');
+
 module.exports = {
   transformer: {
     assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+  },
+  resolver: {
+    assetExts: ['bin', 'txt', 'jpg'],
+    sourceExts: ['js', 'json', 'ts', 'tsx', 'jsx'],
+    blacklistRE: blacklist([/platform_node/])
   },
 };
