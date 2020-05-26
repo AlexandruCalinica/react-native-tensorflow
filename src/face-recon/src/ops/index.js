154801959545ba1dc6702a86c385470936fbd7b6
AlexandruCalinica <calinicaalexandru@gmail.com>
2020-05-26 15:35:31 +0300

    Ported ops directory from ts to js

--- src/face-recon/src/ops/index.js
+++ src/face-recon/src/ops/index.js
@@ -1,2 +1,14 @@
+export * from './iou'
 export * from './minBbox'
+export * from './nonMaxSuppression'
+export * from './normalize'
 export * from './padToSquare'
+export * from './shuffleArray'
+
+export function sigmoid(x) {
+  return 1 / (1 + Math.exp(-x))
+}
+
+export function inverseSigmoid(x) {
+  return Math.log(x / (1 - x))
+}
