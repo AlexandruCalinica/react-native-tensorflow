69ec40e9412a04afcd1e4940707f2db3455595d2
AlexandruCalinica <calinicaalexandru@gmail.com>
2020-05-26 11:41:28 +0300

    Finished porting the classes directory from ts ro js

---
+++ src/face-recon/src/classes/FaceMatch.js
@@ -0,0 +1,16 @@
+
+import { round } from '../utils';
+
+export class FaceMatch {
+  constructor(label, distance) {
+    this._label = label;
+    this._distance = distance;
+  }
+
+  get label() { return this._label }
+  get distance() { return this._distance }
+
+  toString(withDistance = true) {
+    return `${this.label}${withDistance ? ` (${round(this.distance)})` : ''}`
+  }
+}
