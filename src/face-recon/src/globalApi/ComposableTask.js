9ce8cf68e121413ef1540b73b6de161357251b69
AlexandruCalinica <calinicaalexandru@gmail.com>
2020-05-28 16:01:42 +0300

    Started porting globalApi

---
+++ src/face-recon/src/globalApi/ComposableTask.js
@@ -0,0 +1,11 @@
+
+export class ComposableTask {
+
+  async then(onfulfilled) {
+    return onfulfilled(await this.run())
+  }
+
+  async run() {
+    throw new Error('ComposableTask - run is not implemented')
+  }
+}
